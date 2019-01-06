369154825d570532b798fae3985eaaee9079a0a5
晨风明悟 <2696922797@qq.com>
2019-01-06 10:41:05 +0800

    refactor: 删除旧示例。

--- HTML5/example/simple/website2/index.js
+++
@@ -1,9 +0,0 @@
-const proxyScript = `importScripts('http://localhost:3002/worker.js')`;
-const proxyUrl = window.URL.createObjectURL(new Blob([ proxyScript ], { type: 'text/javascript' }));
-const worker = new Worker('./worker.js');
-console.log('index -> worker');
-worker.postMessage('index -> worker');
-
-worker.onmessage = e => {
-  console.log('index:', e.data);
-};
